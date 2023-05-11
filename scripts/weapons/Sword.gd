16ba59c6ba8541ad9af5eed4c2be51bdd0b3f1bd
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2023-05-11 11:30:02 +0200

    Sword works good enough

--- scripts/weapons/Sword.gd
+++ scripts/weapons/Sword.gd
@@ -10,16 +10,20 @@ func _ready():
 	super._ready()
 
 func use():
-#	play("swing")
 	animation_player.play("swing")
 	
 
 
-func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	print(body_rid)
-	print(body)
+#func _on_area_2d_body_shape_entered(_body_rid, _body, _body_shape_index, _local_shape_index):
+#	print("_on_area_2d_body_shape_entered")
 
-	print(body_shape_index)
-	print(local_shape_index)
-	print("ha")
-	pass # Replace with function body.
+
+func _on_area_2d_body_entered(_body):
+	print(_body)
+#	print("_on_area_2d_body_entered")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	match anim_name:
+		"swing": animation_player.play("idle")
+		
